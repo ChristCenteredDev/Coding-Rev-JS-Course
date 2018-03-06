43cf5e67ae132379970250d819f20d1c9ba20bda
William M <wmooney1984@gmail.com>
2018-03-06 10:37:28 -0500

    Section 1 - Variables - Completed

--- Section 01/app.js
+++ Section 01/app.js
@@ -1,3 +1,9 @@
 // External JS Script that get's linked to HTML Document
 
+// Variables
+var newVar = "Ron"; // String
+var bool = true; // Boolean
+var num = 23; // Number
+var arr = ["Ted", "Frank"]; // Array
 
+console.log("Hello " + newVar);
