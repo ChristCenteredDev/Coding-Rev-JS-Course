3baaa057f69c53f217dcd1948622904b25378605
William M <wmooney1984@gmail.com>
2018-03-06 10:55:13 -0500

    Section 2 - Arrays - Completed

--- Section 02/app.js
+++ Section 02/app.js
@@ -1,8 +1,14 @@
 // External JS Script that get's linked to HTML Document
 
+//Numbers and Bools
 var num = 23; // Number
 var bool = true; // Boolean
 
 if (bool != false) {
     console.log(num);
 }
+
+// Arrays
+var arr = ["Ted", "Frank", "Chris", "Chad"];
+
+console.log(arr[1]);
