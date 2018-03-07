314434c093860747b3c86255442ba88cf9a5bb06
William M <wmooney1984@gmail.com>
2018-03-07 10:36:28 -0500

    Section 3 - Delete an Element - Completed

--- Section 03/Lecture 40/app.js
+++ Section 03/Lecture 40/app.js
@@ -1,32 +1,21 @@
-// Window & DOM
-console.log(window);
-console.log(window.innerWidth);
-console.log(window.outerHeight);
-console.log(window.open("http://www.google.com"));
-
-// Document Object
-console.log(document.title);
-console.log(document.body.children[0]);
-
-// Selecting DOM Elements
-console.log(document.getElementById("different")); // Older DOM Method
-console.log(document.getElementsByClassName("differ")); // Older DOM Method
-console.log(document.querySelector("#different")); // Mordern DOM Method
-console.log(document.querySelectorAll("div")); // Mordern DOM Method
-
 // Create an Element
-var div = document.createElement("div");
-div.textContent = "My new content";
-div.style.fontSize = "20px";
+// var div = document.createElement("div");
+// div.textContent = "My new content";
+// div.style.fontSize = "20px";
 
-console.log(div);
+// console.log(div);
 
 // Insert an Element
+// var parent = document.querySelector(".parent");
+
+// var div = document.createElement("div");
+// div.textContent = "My new content";
+// div.style.fontSize = "20px";
+
+// parent.appendChild(div);
+
+// Delete an Element
 var parent = document.querySelector(".parent");
+var ul = parent.querySelector("ul");
 
-var div = document.createElement("div");
-div.textContent = "My new content";
-div.style.fontSize = "20px";
-
-parent.appendChild(div);
-
+parent.removeChild(ul);  // ul.remove() - works but only on modern browsers
