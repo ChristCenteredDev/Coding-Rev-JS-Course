a6b9bf96b7c5499444a0bba985c052187e283d5c
William M <wmooney1984@gmail.com>
2018-03-07 14:31:27 -0500

    Section 3 - Event Listeners - Completed

--- Section 03/Lecture 41/app.js
+++ Section 03/Lecture 41/app.js
@@ -1,6 +1,7 @@
 // Events
 var button = document.querySelector(".myButton");
 
+/* 
 button.onclick = function () {
     console.log("Someone clicked me");
 }
@@ -16,3 +17,19 @@ button.ondblclick = function () {
 button.onmousedown = function () {
     console.log("Someone mouse downed me");
 }
+*/
+
+// Event Listeners
+function click1() {
+    console.log("Someone clicked me");
+}
+
+function click2() {
+    console.log("Something else");
+    button.removeEventListener("click", click1);
+}
+
+button.addEventListener("click", click1);
+button.addEventListener("click", click2);
+
+
