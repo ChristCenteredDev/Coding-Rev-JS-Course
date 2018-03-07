e6ee307750a439c3c032fb8c328db7da8cbcb2b5
William M <wmooney1984@gmail.com>
2018-03-07 09:25:57 -0500

    Section 3 - Document Object & Selecting DOM Elements - Completed

--- Section 03/app.js
+++ Section 03/app.js
@@ -5,3 +5,13 @@ console.log(window.outerHeight);
 console.log(window.open("http://www.google.com"));
 
 // Document Object
+console.log(document.title);
+console.log(document.body.children[0]);
+
+// Selecting DOM Elements
+console.log(document.getElementById("different")); // Older DOM Method
+console.log(document.getElementsByClassName("differ")); // Older DOM Method
+console.log(document.querySelector("#different")); // Mordern DOM Method
+console.log(document.querySelectorAll("div")); // Mordern DOM Method
+
+// Create an Element
