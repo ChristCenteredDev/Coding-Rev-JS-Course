08f9c485edca9b53ea91f381093a8bfe85739cb8
William M <wmooney1984@gmail.com>
2018-03-09 12:25:16 -0500

    Section 7 - Card Generator - Add New User

--- Section 07/CardsGenerator/js/app.js
+++ Section 07/CardsGenerator/js/app.js
@@ -1,40 +1,56 @@
 var db = [
     {name: "James Butters", email: "James@gmail.com", age: 15},
     { name: "Mark Burton", email: "Mark@gmail.com", age: 23},
-    { name: "Todd Jenning", email: "Todd@gmail.com", age: 18}
+    { name: "Lisa Barbosa", email: "Lisa@gmail.com", age: 18}
 ];
 
 (function Avatars(db) {
     this.init = function () {
         this.generateList();
+        this.enterUser();
     }
 
     this.generateList = function () {
-
-        var parent = document.querySelector("#parent_avatars");
-        var template = "";
+        var parent = document.querySelector('#parent_avatars');
+        var template = '';
 
         for (var i = 0; i < db.length; i++) {
-
-            template += '<div class="col- sm - 4">';
-            template +=     '<div class="card">';
-            template +=         '<div class="card- delete " data-card="">x</div>';
-            template +=         '<div class="card- block">';
-            template +=             '<h3 class="card-title">'+ db[i].name +'</h3>';
-            template +=             '<p class="card-text"><strong>Email:</strong>' + db[i].email + '</p>';
-            template +=             '<p class="card-text"><strong>Age:</strong>' + db[i].age + '</p>';
-            template +=        '</div>';
-            template +=    '</div>';
+            template += '<div class="col-sm-4">';
+            template += '<div class="card">';
+            template += '<div class="card-delete" data-card="' + i + '">x</div>';
+            template += '<div class="card-block">';
+            template += '<h3 class="card-title">' + db[i].name + '</h3>';
+            template += '<p class="card-text"><strong>Email:</strong> ' + db[i].email + '</p>';
+            template += '<p class="card-text"><strong>Age:</strong> ' + db[i].age + '</p>';
             template += '</div>';
-
+            template += '</div>';
+            template += '</div>';
         }
-
         parent.innerHTML = '';
         parent.insertAdjacentHTML('afterbegin', template);
 
+    };
 
-    }
+    this.enterUser = function () {
 
+        function grabUser() {
+            var name = document.querySelector('#user_name').value;
+            var email = document.querySelector('#user_email').value;
+            var age = document.querySelector('#user_age').value;
+
+            document.querySelector('#myForm').reset();
+
+            db.push({ name: name, email: email, age: age });
+            generateList();
+        }
+
+        document.querySelector('#myForm').addEventListener("submit", function (e) {
+            e.preventDefault();
+            grabUser();
+
+        });
+
+    };
 
     this.init();
 })(db);
