af3e5c3388585ceaf0d0a653b68847c8d22b69b4
William M <wmooney1984@gmail.com>
2018-03-08 20:28:03 -0500

    Section 6 - Objects, This Keyword & Prototypes - Completed

--- Section 06/Lectures 70-76/app.js
+++ Section 06/Lectures 70-76/app.js
@@ -1,3 +1,41 @@
-// External JS Script that get's linked to HTML Document
+// Objects
 
+var user = {
+    name: "Francis",
+    lastName: "Jones",
+    age: 16,
+    cars: ["Car", "Truck", "Motorcycle", "Moped"],
+    misc: {
+        hobbies: ["Driving", "Games", "Church"],
+        lang: ["English", "Filipino"]
+    },
+    salute: function () {
+        console.log("Hi there, I am " + this.name + ".  I am " + this.age);  // This Keyword - is working
+    }
+};
 
+console.log(user);
+console.log(user.misc.lang);
+user.salute();
+
+// This Keyword
+
+// Using Prototype
+
+function User() {
+
+}
+
+var user = new User();
+user.name = "Max";
+
+Object.prototype.scream = function() {
+    console.log("A scream");
+}
+
+user.scream();
+
+console.log(user);
+console.log(user.__proto__);
+
+// Prototypes in real life
