c11849432a9605a0e6685e89f4d5af4fe0224d93
William M <wmooney1984@gmail.com>
2018-03-10 12:58:13 -0500

    Section 8 - Who Pays Bill - JS Functionality & Validation Added

--- Section 08/WhoPaysTheBill/app.js
+++ Section 08/WhoPaysTheBill/app.js
@@ -1,3 +1,58 @@
-// External JS Script that get's linked to HTML Document
+
+
+(function GetLoser() {
+    this.applicants = [];
+
+    this.init = function () {
+        this.addApplicants();
+    };
+
+    this.addApplicants = function () {
+
+        function generateList(input) {
+            var value = input.value;
+
+
+            //  Modify Below Code
+
+            if (validateUser(value)) {
+                applicants.push(value);
+                input.value = "";
+                console.log(applicants);
+
+            } else {
+
+                document.querySelector("#error").style.display = "block";
+                setTimeout(function () {
+                    document.querySelector("#error").style.display = "none";
+                }, 2000);
+
+            }
+
+            //  Modify Above code
+
+
+        }
+
+        var addBtn = document.querySelector("#add_applicant");
+
+        addBtn.addEventListener("click", function () {
+            var input = document.querySelector("#applicant_value");
+            generateList(input);
+        });
+
+
+    };
+
+    this.validateUser = function (value) {
+        if (applicants.indexOf(value) >= 0 || value == '') {
+            return false;
+        }
+
+        return true;
+    }
+
+    this.init();
+})();
 
 
