42de6e013898a0db8637e24aad6f5c61c8e5eee0
William M <wmooney1984@gmail.com>
2018-03-10 14:25:10 -0500

    Section 8 - Who Pays Bill - Completed

--- Section 08/WhoPaysTheBill/app.js
+++ Section 08/WhoPaysTheBill/app.js
@@ -5,6 +5,23 @@
 
     this.init = function () {
         this.addApplicants();
+        this.getRandomUser();
+        this.runAgain();
+        this.startOver();
+    };
+
+    this.showList = function () {
+        var parent = document.querySelector('.applicant_list_wrapper');
+        var template = '';
+
+        for (var i = 0; i < applicants.length; i++) {
+            template += '<span class="name-tag" data-id="' + i + '">' + applicants[i] + '</span>'
+        }
+
+        parent.innerHTML = '';
+        parent.insertAdjacentHTML('afterbegin', template);
+        deleteOne();
+
     };
 
     this.addApplicants = function () {
@@ -23,27 +40,8 @@
                 setTimeout(function () {
                     document.querySelector("#error").style.display = "none";
                 }, 2000);
-
             }
-
-
         }
-
-        function showList() {
-            var parent = document.querySelector('.applicant_list_wrapper');
-            var template = '';
-
-            for (var i = 0; i < applicants.length; i++) {
-                template += '<span class="name-tag" data-id="' + i + '">' + applicants[i] + '</span>'
-            }
-
-            parent.innerHTML = '';
-            parent.insertAdjacentHTML('afterbegin', template);
-            deleteOne();
-
-        }
-
-
 
         var addBtn = document.querySelector("#add_applicant");
 
@@ -62,6 +60,82 @@
 
         return true;
     }
+
+    this.deleteOne = function () {
+        var item = document.querySelectorAll('.name-tag');
+        function removeIt(element) {
+            var attr = parseInt(element.getAttribute('data-id'));
+
+            applicants.splice(attr, 1);
+            showList();
+        }
+
+        for (var i = 0; i < item.length; i++) {
+            item[i].addEventListener('click', function (e) {
+                removeIt(this);
+            });
+        }
+    };
+
+    this.getRandomUser = function () {
+        var resultsButton = document.querySelector('#show_results');
+
+        function showLooser() {
+            var resultsContainer = document.querySelector('.results_container');
+            var applicantContainer = document.querySelector('.applicant_container');
+
+            applicantContainer.className += ' hidden';
+            resultsContainer.className = 'results_container';
+
+            showRandomUser();
+        }
+
+        resultsButton.addEventListener('click', function (e) {
+            if (applicants.length > 1) {
+                showLooser();
+            } 
+            else {
+
+                document.querySelector("#error2").style.display = "block";
+                setTimeout(function () {
+                    document.querySelector("#error2").style.display = "none";
+                }, 2000);
+            }
+
+        });
+    };
+
+    this.showRandomUser = function () {
+        var resultContainer = document.querySelector('.result');
+        var rand = applicants[Math.floor(Math.random() * applicants.length)];
+
+        resultContainer.innerHTML = '';
+        resultContainer.insertAdjacentHTML('afterbegin', '<h3>' + rand + '</h3>');
+    };
+
+    this.runAgain = function () {
+        var runAgainBtn = document.querySelector('.run_again');
+
+        runAgainBtn.addEventListener('click', function (e) {
+            showRandomUser();
+        });
+    };
+
+    this.startOver = function () {
+        var startAgainBtn = document.querySelector('.start_again');
+
+        startAgainBtn.addEventListener('click', function (e) {
+            var resultsContainer = document.querySelector('.results_container');
+            var aplicantContainer = document.querySelector('.applicant_container');
+            var aplicantWrapper = document.querySelector('.applicant_list_wrapper');
+
+            aplicantContainer.className = 'applicant_container';
+            resultsContainer.className = 'results_container hidden';
+            aplicantWrapper.innerHTML = '';
+
+            applicants = [];
+        });
+    };
 
     this.init();
 })();
