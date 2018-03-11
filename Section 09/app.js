9def7c063cab4099a3417dd4bd4b444f548b6d00
William M <wmooney1984@gmail.com>
2018-03-11 15:23:50 -0400

    Section 9 - HTTP Request "GET" - Completed

--- Section 09/app.js
+++ Section 09/app.js
@@ -1,3 +1,16 @@
-// External JS Script that get's linked to HTML Document
 
 
+var http = new XMLHttpRequest();
+var url = "https://jsonplaceholder.typicode.com/posts";
+var method = "GET";
+
+http.open(method, url);
+http.onreadystatechange = function () {
+    if (http.readyState === XMLHttpRequest.DONE && http.status === 200) {
+        console.log(JSON.parse(http.response()));
+    } else if (http.readyState === XMLHttpRequest.DONE && http.status !== 200) {
+        console.log("bad something..")
+    }
+};
+
+http.send();
