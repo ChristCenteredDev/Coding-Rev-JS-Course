140a81ecf88471680b53fd33d33cb87fede58051
William M <wmooney1984@gmail.com>
2018-03-12 21:44:43 -0400

    Section 10 - Music DB - Initial JavaScript

--- Section 10/MusicDB/app.js
+++ Section 10/MusicDB/app.js
@@ -1,3 +1,20 @@
-// External JS Script that get's linked to HTML Document
+(function musicDB() {
+    this.init = function () {
+        this.search();
+    }
 
+    this.search = function () {
+        var form = document.querySelector("#form");
 
+        form.addEventListener("submit", function (e) {
+            e.preventDefault();
+
+            // Lecture 102 - 2:50 minutes
+
+        })
+
+    }
+
+    this.init();
+
+})();
