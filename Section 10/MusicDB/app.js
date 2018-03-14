0c141a5010309b8dc06a19be9c9c95f249532f59
William M <wmooney1984@gmail.com>
2018-03-14 12:20:30 -0400

    Section 10 - Music DB - Search Functionality

--- Section 10/MusicDB/app.js
+++ Section 10/MusicDB/app.js
@@ -11,20 +11,23 @@
             var value = document.querySelector("#input_search").value;
             form.reset();
 
-            getData(value);
+            getData(value.split(" ").join("+"));
         })
 
     }
 
     this.getData = function (artist) {
         var http = new XMLHttpRequest();
-        var url = "https://itunes.apple.com/search?term=audioslave&entity=album";
+        var url = "https://itunes.apple.com/search?term=" + artist + "&entity=album";
         var method = "GET";
+
+        var container = document.querySelector("#album_list_container");
+        container.innerHTML = "";
 
         http.open(method, url);
         http.onreadystatechange = function () {
             if (http.readyState === XMLHttpRequest.DONE && http.status === 200) {
-                console.log(JSON.parse(http.response));
+                showArtist(JSON.parse(http.response));
 
             } else if (http.readyState === XMLHttpRequest.DONE && http.status !== 200) {
 
@@ -33,6 +36,13 @@
         }
 
         http.send();
+    };
+
+    this.showArtist = function (obj) {
+        var container = document.querySelector("#album_list_container");
+        var template = "";
+
+
     };
 
 
