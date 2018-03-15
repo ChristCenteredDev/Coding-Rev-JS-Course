0a59a5ad0fc0024a101d3be54a8ba05a6d78f3b7
William M <wmooney1984@gmail.com>
2018-03-15 13:00:33 -0400

    Resources Updated

--- Section 12/app.js
+++ Section 12/app.js
@@ -1,19 +1,3 @@
-var nums = [6, -5, 2];
-var letters = a, b, c
-
-console.log(nums.sort);
-
-var arr = [];
+// External JS Script that get's linked to HTML Document
 
 
-
-
-
-
-for (var i = 0; i < nums.length; i++) {
-    if (nums[i] < nums[i + 1]) {
-        arr.push(nums[i]);
-    } else if (nums[i + 1] || nums[i + 1] == 0) {
-        arr.push(nums[i + 1]);
-    }
-}
