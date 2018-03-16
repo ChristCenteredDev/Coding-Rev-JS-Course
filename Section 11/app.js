6e46c60c7867b7897253f7d61b09ff9d77f4d52a
William M <wmooney1984@gmail.com>
2018-03-16 16:25:59 -0400

    Section 11 - Scope w/ Let & Const - Completed

--- Section 11/app.js
+++ Section 11/app.js
@@ -1,6 +1,18 @@
+// Let and Const
 const type = "Cupcake";
 let flavor = "Apple";
 let price = 5;
 
 type = "Cake" // Error due to Const not allowing change
 flavor = "Butter Pecan"; // Works fine with let
+
+// ES6 Scope w/ Let & Const
+var winner = true; // Global
+
+function user() {
+    var winner = false; // Local - This needs to be console.log in the function
+} // Note: With Let even in a if it has local scope
+
+user();
+
+console.log(winner); // true - Global variable shows
