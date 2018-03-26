c9aa145023d168ac5145b7d38edc806b56232776
William M <wmooney1984@gmail.com>
2018-03-25 20:43:25 -0400

    Section 12 - ForEach Helper - Examples

--- Section 12/app.js
+++ Section 12/app.js
@@ -1,3 +1,27 @@
-// External JS Script that get's linked to HTML Document
+// Section 12 - ES6 Using Helpers
+
+// forEach
+
+var artists = ["Clapton", "U2", "Lamar"];
+
+for (var i = 0; i < artists.length; i++) {
+    console.log(artists[i]);
+}
+
+artists.forEach(function (artist) {
+    console.log(artist);
+});
+
+artists.forEach((artist) => {
+    console.log(artist);
+});
 
 
+const names = ["Francis", "Lisa", "Ron"];
+let template = "";
+
+names.forEach(function(name) {
+    template += `<div>Hi my name is ${name} <strong> click me</strong></div>`;
+});
+
+document.body.insertAdjacentHTML("afterbegin", template);
