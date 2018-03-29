dff48989350deee305fae79f6e20fafc97f3ab02
William M <wmooney1984@gmail.com>
2018-03-28 23:35:07 -0400

    Section 12 - Map Helper Exercise 2 - Initial Commit

---
+++ Section 12/Exercise120/app.js
@@ -0,0 +1,9 @@
+/*
+We have a list of cars with a brand and a price, and we want to convert the price to a different currency, and put it in a new array just like  “Ford is 40000 rupies”.
+*/
+
+const cars = [
+    { name: 'Ford', price: 200 },
+    { name: 'Nissan', price: 400 },
+    { name: 'Chevy', price: 600 }
+]
