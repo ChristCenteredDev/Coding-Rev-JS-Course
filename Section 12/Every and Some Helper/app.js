155e0b5c98651961d05ec7b67d9142c9870e1ce2
William M <wmooney1984@gmail.com>
2018-03-29 21:59:52 -0400

    Section 12 - Every and Some Helper Examples - Completed

--- Section 12/Every and Some Helper/app.js
+++ Section 12/Every and Some Helper/app.js
@@ -1,1 +1,19 @@
 // Every and Some Helper
+
+const students = [
+    {name: "Francis", grade: 4},
+    {name: "Martin", grade: 5},
+    {name: "Martha", grade: 7},
+];
+
+const every = students.every(function (student) {
+    return student.grade > 6;
+});
+
+console.log(every); // false
+
+const some = students.some(function (student) {
+    return student.grade > 6;
+});
+
+console.log(some); // true
