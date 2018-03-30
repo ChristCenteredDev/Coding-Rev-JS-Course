493f157c3ff5eb8ed5f1496f6db2d0b0251d5a2d
William M <wmooney1984@gmail.com>
2018-03-30 15:09:08 -0400

    Section 14 - Using Classes - Completed

--- Section 14/app.js
+++ Section 14/app.js
@@ -1,3 +1,14 @@
 // ES6 Classes
 
+function Car() { }
+var car = new Car();
 
+Car.prototype.status = "New";
+Car.prototype.wheels = 4;
+Car.prototype.avail = function () {
+    console.log("available");
+};
+
+var ford = new Car();
+
+console.log(ford.__proto__);
