4670ef3d742acfdb910bd295b4e23f62dba646a3
William M <wmooney1984@gmail.com>
2018-03-29 22:11:05 -0400

    Section 12 - Reduce Helper Examples - Completed

--- Section 12/Reduce Helper/app.js
+++ Section 12/Reduce Helper/app.js
@@ -1,1 +1,26 @@
 // Reduce Helper
+
+const previous = 200;
+const current = [
+    {expense: "Guitar", amount: 200},
+    {expense: "Mic", amount: 100},
+    {expense: "strap", amount: 100},
+];
+
+const results = current.reduce(function (sum, expense) {
+    return sum + expense.amount;
+}, previous);
+
+// Reduce Helper Example 2 with Strings
+const users = [
+    {name: "Francis", lastname: "Norton", age: 25},
+    {name: "Martha", lastname: "Suchard", age: 19},
+    {name: "Rob", lastname: "Maker", age: 21},
+];
+
+const names = users.reduce(function () {
+    start.push(user.name);
+    return start;
+}, []);
+
+console.log(names);  // ["Francis", "Martha", "Rob"];
