471ae23b82a3f4b1e505eb7a8d1da3bfab1d8c7d
William M <wmooney1984@gmail.com>
2018-03-30 17:11:02 -0400

    Section 15 - Destructuring Objects - Completed

--- Section 15/app.js
+++ Section 15/app.js
@@ -1,3 +1,17 @@
-// External JS Script that get's linked to HTML Document
+// Destructuring Objects
 
+var user = {
+    name: "Francis",
+    lastname: "Jones",
+    age: 25,
+};
 
+var name = user.name;
+var lastname = user.lastname;
+var age = user.age;
+
+console.log(name);
+console.log(lastname);
+console.log(age);
+
+const { name, lastname, age } = user;
