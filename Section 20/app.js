4bb3c16d675093d3739bb0ed1eb8135f9083638e
William M <wmooney1984@gmail.com>
2018-03-31 20:26:41 -0400

    Resources Updated

--- Section 20/app.js
+++ Section 20/app.js
@@ -1,3 +1,5 @@
-// External JS Script that get's linked to HTML Document
+// Starting with jQuery - Watched the video's
 
-
+$(document).ready(function () {  // Only runs the JS code once the webpage fully loaded
+    /*  <<< Insert all JS here >>>  */
+});
