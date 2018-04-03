e0b5183762b00aae05c532ac82b87ef93643b554
William M <wmooney1984@gmail.com>
2018-04-02 21:13:37 -0400

    Section 18 - jQuery: Append & Prepend - Completed

--- Section 18/Append-Prepend/app.js
+++ Section 18/Append-Prepend/app.js
@@ -1,5 +1,26 @@
 // Section 18 - jQuery: using the Dom
 
 $(document).ready(function () {  // Only runs the JS code once the webpage fully loaded
-    /*  <<< Insert all JS here >>>  */
+    /*  <<< Append / Prepend >>>  */
+
+    /* Vanilla JavaScript */
+    //var parent = document.querySelector(".parent");
+    //var newElement = document.createElement("div");
+    //newElement.className = "newDiv";
+    //newElement.innerText = "Hello Me!";
+    //parent.appendChild(newElement);
+
+    /* Append jQuery - put to the rear of the element */
+    // $(".parent").append("<div class='newDiv'>Hello Me!</div>");
+
+    /* AppendTo jQuery - put to the rear of the element */
+    // $("<div class='newDiv'>Hello Me!</div>").appendTo(".parent");
+
+    /* Prepend jQuery - put to the front of the element */
+    // $(".parent").prepend("<div class='newDiv'>Hello Me!</div>");
+
+    /* PrependTo jQuery - put to the front of the element */
+    $("<div class='newDiv'>Hello Me!</div>").prependTo(".parent");
+
+
 });
