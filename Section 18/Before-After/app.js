0236baa0c6661c0b1fc593042eac1340c1242054
William M <wmooney1984@gmail.com>
2018-04-03 00:22:44 -0400

    Section 18 - jQuery: Before & After - Completed

--- Section 18/Before-After/app.js
+++ Section 18/Before-After/app.js
@@ -1,5 +1,16 @@
 // Section 18 - jQuery: using the Dom
 
 $(document).ready(function () {  // Only runs the JS code once the webpage fully loaded
-    /*  <<< Insert all JS here >>>  */
+    /*  <<< Before & After >>>  */
+
+    // Before
+    $(".element").before("<div>Before</div>");
+
+    // After
+    $(".element").after("<div>After</div>");
+
+    // Error Message Example
+    $("input").after("<div class='error'>Missing message</div>");
+
+
 });
